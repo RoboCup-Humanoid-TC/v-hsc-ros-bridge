e8f99dd9bd867553e97a0e6e4eced96de269376c
Soheil <soheilkhatibi1377@gmail.com>
2021-05-25 05:11:43 +0430

    [#FEATURE] Publish motor position sensors data.

--- src/ros_bridge/src/webots_controller.cpp
+++ src/ros_bridge/src/webots_controller.cpp
@@ -7,9 +7,14 @@
 #include "std_msgs/msg/string.hpp"
 #include "rosgraph_msgs/msg/clock.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 #include "cv_bridge/cv_bridge.h"
 
 #include "ros_bridge/robot_client/robot_client.hpp"
+
+#include <json/value.h>
+#include <jsoncpp/json/json.h>
+#include <fstream>
 
 using namespace std::chrono_literals;
 
@@ -18,19 +23,35 @@ class WebotsController : public rclcpp::Node
   public:
     WebotsController()
     : Node("webots_controller") {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       clock_publisher_ = this->create_publisher<rosgraph_msgs::msg::Clock>("clock", 10);
       image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("image", 10);
+      sensor_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("sensor", 10);
+
       timer_ = this->create_wall_timer(
       8ms, std::bind(&WebotsController::timer_callback, this));
       client = new RobotClient("127.0.0.1", 10001, 3);
       client->connectClient();
 
-      // Enable camera
       ActuatorRequests request;
+      
+      // Enable camera
       SensorTimeStep *camera_sensor = request.add_sensor_time_steps();
       camera_sensor->set_name("Camera");
       camera_sensor->set_timestep(16);
+      
+      Json::Value motors_json;
+      std::ifstream json_file("src/ros_bridge/src/motors.json");
+      json_file >> motors_json;
+
+      // Enable sensors
+      for (unsigned int i=0; i < motors_json.size(); i++) {
+        // std::cout<< motors_json[i]["name"].asString() << std::endl;
+        // std::cout<< motors_json[i]["time_step"].asDouble() << std::endl;
+        SensorTimeStep *sensor = request.add_sensor_time_steps();
+        sensor->set_name(motors_json[i]["name"].asString() + "S");
+        sensor->set_timestep(motors_json[i]["time_step"].asDouble());
+      }
+
       client->sendRequest(request);
       SensorMeasurements sensors = client->receive();
     }
@@ -44,6 +65,7 @@ class WebotsController : public rclcpp::Node
           client->sendRequest(request);
           SensorMeasurements sensors = client->receive();
           publishImage(sensors);
+          publishSensors(sensors);
         } catch (const std::runtime_error &exc) {
           std::cerr << "Runtime error: " << exc.what() << std::endl;
         }
@@ -56,7 +78,7 @@ class WebotsController : public rclcpp::Node
         if (sensor_data.quality() != -1) {
           throw std::runtime_error("Encoded images are not supported in this client");
         }
-        
+
         cv::Mat img(sensor_data.height(),sensor_data.width(), CV_8UC3, (void*)sensor_data.image().c_str());
 
         auto imgmsg = sensor_msgs::msg::Image();
@@ -67,10 +89,19 @@ class WebotsController : public rclcpp::Node
       }
     }
 
+    void publishSensors(const SensorMeasurements& sensors){
+      auto jointmsg = sensor_msgs::msg::JointState();
+      for (int i=0; i < sensors.position_sensors_size(); i++) {
+        jointmsg.name.push_back(sensors.position_sensors(i).name());
+        jointmsg.position.push_back(sensors.position_sensors(i).value());
+      }
+      sensor_publisher_->publish(jointmsg);
+    }
+
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     rclcpp::Publisher<rosgraph_msgs::msg::Clock>::SharedPtr clock_publisher_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr sensor_publisher_;
     RobotClient* client;
 };
 
