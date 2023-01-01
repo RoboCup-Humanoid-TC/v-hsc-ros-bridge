f7e18e09fa1543575f6d0cf039ea03e718e14637
Florian Vahl <git@flova.de>
2023-01-01 19:04:16 +0100

    Fix cv_bridge deprication warning

--- src/ros_bridge/src/webots_controller.cpp
+++ src/ros_bridge/src/webots_controller.cpp
@@ -13,7 +13,7 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <sensor_msgs/msg/imu.hpp>
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 
 #include "ros_bridge/robot_client/robot_client.hpp"
 
